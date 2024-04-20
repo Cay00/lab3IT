b753762881388d3695aa8e1afc6560fba73b0863
Krzysztof <krzysztof.turek007@gmail.com>
2024-04-20 23:12:15 +0200

    Odpowiedzi

---
+++ odpowiedz.sh
@@ -0,0 +1,137 @@
+git commit
+git commit
+
+git branch bugFix
+git checkout bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git merge bugFix
+
+git checkout -b bugFix
+git commit
+git checkout main
+git commit
+git checkout bugFix
+git rebase main
+
+git checkout C4
+
+git checkout C3
+
+git branch -f main C6
+git branch -f bugFix C0
+git checkout C1
+
+git reset HEAD^
+git checkout pushed
+git revert pushed
+
+git cherry-pick C3 C4 C7
+
+git rebase -i C1
+
+git checkout main
+git cherry-pick C4
+
+git rebase -i C1
+git rebase -i C3'
+git rebase -i C1
+git rebase caption main
+
+git checkout C1
+git cherry-pick C2
+git commit --amend
+git cherry-pick C3
+git rebase HEAD main
+
+git tag v0 C1
+git tag v1 C2
+git checkout C2
+
+git commit
+
+git checkout bugFix
+git rebase main
+git checkout side
+git rebase -i bugFix
+git checkout another
+git rebase side
+git checkout main
+git merge another
+
+git branch bugWork HEAD~^2^
+
+git branch -f three HEAD~3
+git rebase -i C1
+git branch -f one
+git checkout C1
+git cherry-pick C5 C4' C3' C2'
+git branch -f two
+git branch -f main C5
+
+git clone
+
+git commit
+git checkout o/main
+git commit
+
+git fetch
+
+git pull
+
+git clone
+git fakeTeamwork 2
+git commit
+git pull
+
+git commit
+git commit
+git push
+
+git clone
+git fakeTeamwork
+git commit
+git pull --rebase
+git push
+
+git reset --hard o/main
+git checkout -b feature C2
+git push
+
+git rebase side1 side2
+git rebase side2 side3
+git rebase side3 main
+git pull --rebase
+git push
+
+git checkout main
+git pull
+git merge side1
+git merge side2
+git merge side3
+git push
+
+git checkout -b side o/main
+git commit
+git pull --rebase
+git push
+
+git push origin main
+git push origin foo
+
+git push origin main~1:foo
+git push origin foo:main
+
+git fetch origin main~1:foo
+git fetch origin foo:main
+git checkout foo
+git merge c6
+
+git push origin :foo
+git fetch origin :bar
+
+git pull origin bar:foo
+git pull origin main:side
